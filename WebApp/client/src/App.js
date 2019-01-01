af582587ab4d1e52b217afc4113dec91175a7cdc
FlorianTh2 <s0558101@htw-berlin.de>
2019-01-01 22:01:14 +0100

    Added React-Client, Set up npm-packages, Added npm instruction npm run dev,  Added Navbar

---
+++ WebApp/client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import AppNavbar from "./components/AppNavbar"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+        <AppNavbar />
+        
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ WebApp/client/src/components/AppNavbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Container
+} from "reactstrap";
+
+// since we dont bind custom functionalities like toggle via bind (instead we go with arrow-functions), we can ommit the constructor
+class AppNavbar extends Component{
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render(){
+        return (
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Page - List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/FlorianTh2/SemtanticSearch-SearchEngine">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        ); 
+    }
+}
+
+
+export default AppNavbar
