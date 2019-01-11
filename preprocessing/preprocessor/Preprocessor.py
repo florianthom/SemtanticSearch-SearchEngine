33d242882b5f9295a7e18cf2bf05f01afbe9d178
Unknown <michael_schwabe@mailbox.org>
2019-01-11 09:27:22 +0100

    some fixes

--- preprocessing/preprocessor/Preprocessor.py
+++ preprocessing/preprocessor/Preprocessor.py
@@ -22,6 +22,22 @@ class Preprocessor:
             self.tagger = pickle.load(f)
         self.stop_words = stopwords.words('german')
 
+        self.umlaute_dict = {
+            '\xc3\xa4': 'ae',  # U+00E4	   \xc3\xa4
+            '\xc3\xb6': 'oe',  # U+00F6	   \xc3\xb6
+            '\xc3\xbc': 'ue',  # U+00FC	   \xc3\xbc
+            '\xc3\x84': 'Ae',  # U+00C4	   \xc3\x84
+            '\xc3\x96': 'Oe',  # U+00D6	   \xc3\x96
+            '\xc3\x9c': 'Ue',  # U+00DC	   \xc3\x9c
+            '\xc3\x9f': 'ss',  # U+00DF	   \xc3\x9f
+        }
+
+    def replace_german_umlaute(self,unicode_string):
+        utf8_string = unicode_string.encode('utf-8')
+        for k in self.umlaute_dict.keys():
+            utf8_string = utf8_string.replace(k, self.umlaute_dict[k])
+        return utf8_string.decode()
+
     def tokenizing_with_sw_and_punc(self,text):
         text = text.lower()
         # tokenize text 
@@ -37,7 +53,8 @@ class Preprocessor:
         filtered_sentence = [w for w in word_tokens if not w in self.stop_words]
         filtered_sentence = []
         for w in word_tokens:
-            if w not in self.stop_words:
+            if w not in self.stop_words:#
+                #str(self.replace_german_umlaute(w))
                 filtered_sentence.append(w)
         filtered_word_tokens = filtered_sentence
         return filtered_word_tokens
@@ -58,7 +75,7 @@ class Preprocessor:
     def lemmating(self,words):
         return self.tagger.tag(words)
 
-
+   
 
     
 
