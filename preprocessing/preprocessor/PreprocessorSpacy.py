8acd679d1f82afff82348024ef2c7a58e0d9f7f5
M4r3l486 <marcelkleint@yahoo.de>
2019-02-04 20:47:02 +0100

    finalized PreprocessorSpaCy

--- preprocessing/preprocessor/PreprocessorSpacy.py
+++ preprocessing/preprocessor/PreprocessorSpacy.py
@@ -1,37 +1,39 @@
 import spacy
 nlp = spacy.load("de")
 
-class PreprocessorSpacy:
-    def replace_german_umlaute(self,string_text):
-        chars = {'ö':'oe','Ö':'Oe','Ä':'Ae','ä':'ae','Ü':'Ue','ü':'ue','ß':'ss'} # usw.
-        for char in chars:
-            string_text = string_text.replace(char,chars[char])
-        return string_text
+def replace_german_umlaute(string_text):
+    chars = {'ö':'oe','Ö':'Oe','Ä':'Ae','ä':'ae','Ü':'Ue','ü':'ue','ß':'ss'} # usw.
+    for char in chars:
+        string_text = string_text.replace(char,chars[char])
+    return string_text
 
-    def tokenizing_with_sw_and_punc(self, text):
-        text = self.replace_german_umlaute(text)
-        # tokenize text
-        return [token.text for token in nlp(text)]
+def tokenizing_with_sw_and_punc(text):
+    text = replace_german_umlaute(text.lower())
+    return [token.text for token in nlp(text)]
 
-    def tokenizing_without_punc(self, text):
-        tokenizer = [token.text for token in nlp(text) if token.is_punct != True]
-        return self.replace_german_umlaute(self.tokenizing_reverse(tokenizer))
+def tokenizing_without_punc(text):
+    text = replace_german_umlaute(text.lower())
+    return[token.text for token in nlp(text) if token.is_punct != True]
 
-    def tokenizing_without_punc_and_sw_with_umlaute(self, text):
-        return[ token.text for token in nlp(text) if token.is_punct != True and token.is_stop != True]
+def tokenizing_without_punc_and_sw_with_umlaute(text):
+    return[ token.text for token in nlp(text.lower()) if token.is_punct != True and token.is_stop != True]
 
-    def tokenizing_without_punc_with_umlaute_sw(self, text):
-        return [ token.text for token in nlp(text) if token.is_punct != True]
+def tokenizing_without_punc_with_umlaute_sw(text):
+    return [ token.text for token in nlp(text.lower()) if token.is_punct != True]
 
-    def tokenizing_complete(self, text):
-        tokenizer = [ token.text for token in nlp(text) if token.is_punct != True and token.is_stop != True]
-        return self.replace_german_umlaute(self.tokenizing_reverse(tokenizer))
+def tokenizing_complete(text):
+    text = replace_german_umlaute(text)
+    return [token.text for token in nlp(text.lower()) if token.is_punct != True and token.is_stop != True]
 
-    def tokenizing_reverse(self, words):
-        return ' '.join(words)#nltk.Text(words)
+def tokenizing_reverse(words):
+    return ' '.join(words)#nltk.Text(words)
 
-    def stemming_words(self, word_tokens):
-        pass
+def stemming(word):
+    pass
 
-    def lemmating(self, words):
-        return [token.lemma_ for token in nlp(words)]
+def stemming_words(word_tokens):
+    pass
+
+def lemmating(words):
+    words = tokenizing_reverse(words)
+    return [token.lemma_ for token in nlp(words)]
